f3ec9fbaf52537431209aeb63485692b21f62453
binur-torrent <binur20072305@gmail.com>
2026-05-18 20:05:31 +0300

    Prisma module and service added

--- apps/backend/src/products/products.service.ts
+++ apps/backend/src/products/products.service.ts
@@ -1,6 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { PrismaClient } from "./generated/prisma/client.js";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+
+
+@Injectable()
+export class PrismaService extends PrismaClient {
+  constructor() {
+    const adapter = new PrismaPg({
+      connectionString: process.env.DATABASE_URL as string,
+    });
+    super({adapter});
+  }
+}
 
 @Injectable()
 export class ProductsService {
